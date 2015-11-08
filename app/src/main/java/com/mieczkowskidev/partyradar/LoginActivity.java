237700ef559684805843419149fc62070b59a574
Patryk Mieczkowski <mieczkowskidev@gmail.com>
2015-11-08 15:21:37 +0100

    Initial commit

---
+++ app/src/main/java/com/mieczkowskidev/partyradar/LoginActivity.java
@@ -0,0 +1,18 @@
+package com.mieczkowskidev.partyradar;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * A login screen that offers login via email/password.
+ */
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+    }
+}
+
